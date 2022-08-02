592313a5686302c61aec439d09975d9b44cbe715
ArCiGo <armandocifuentes_1@hotmail.com>
2022-08-02 11:56:13 -0500

    Create of automation framework for Sauce Demo portal

---
+++ cypress/e2e/logout.spec.cy.js
@@ -0,0 +1,13 @@
+describe('SWAG Labs logout', () => {
+    
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/');
+        cy.loginForm(Cypress.env('valid_user').username, Cypress.env('valid_user').password);
+    });
+
+    it('should logout to the application', () => {
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#logout_sidebar_link').click();
+        cy.get('.login_logo').should('be.visible');
+    });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,79 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginForm', (username, password) => {
+    if(username.length !==  0) {
+        cy.get('#user-name').clear().type(username);
+    } else {
+        cy.get('#user-name').clear().type('{backspace}');
+    }
+
+    if(password.length !== 0) {
+        cy.get('#password').clear().type(password);
+    } else {
+        cy.get('#password').clear().type('{backspace}');
+    }
+    
+    cy.get('#login-button').click();
+});
+
+Cypress.Commands.add('checkoutForm', (firstName, lastName, postalCode) => {
+    if(firstName.length !== 0) {
+        cy.get('#first-name').clear().type(firstName);
+    } else {
+        cy.get('#first-name').clear().type('{backspace}');
+    }
+
+    if(lastName.length !== 0) {
+        cy.get('#last-name').clear().type(lastName);
+    } else {
+        cy.get('#last-name').clear().type('{backspace}');
+    }
+
+    if(postalCode.length !== 0) {
+        cy.get('#postal-code').clear().type(postalCode);
+    } else {
+        cy.get('#postal-code').clear().type('{backspace}');
+    }
+
+    cy.get('#continue').click();
+});
+
+Cypress.Commands.add('addProductsToCart', () => {
+    /*products.forEach((item) => {
+        cy.contains('.inventory_item', item)
+        .find('button[class*="btn btn_primary"]')
+        .click();
+    });*/
+
+    cy.fixture("products")
+        .then((products) => {
+            products.forEach(item => {
+                cy.contains('.inventory_item', item)
+                .find('button[class*="btn btn_primary"]')
+                .click();     
+            });
+        });
+});
