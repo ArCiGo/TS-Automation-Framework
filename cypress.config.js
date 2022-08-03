aa3786e108281084fb2e97c11980236c13180e1e
ArCiGo <armandocifuentes_1@hotmail.com>
2022-08-03 16:06:39 -0500

    Add of video attribute

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    chromeWebSecurity: false
+    chromeWebSecurity: false,
+    video: false
   },
 });
